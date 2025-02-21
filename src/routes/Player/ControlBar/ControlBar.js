ba469ddfaf67441e894087478bc1d383bcbab2ad
Zarg <62082797+Zaarrg@users.noreply.github.com>
2025-02-21 16:10:05 +0100

    updated network control bar icon to not be shown if unavailable

--- src/routes/Player/ControlBar/ControlBar.js
+++ src/routes/Player/ControlBar/ControlBar.js
@@ -146,9 +146,22 @@ const ControlBar = ({
                     <Icon className={styles['icon']} name={'more-vertical'} />
                 </Button>
                 <div className={classnames(styles['control-bar-buttons-menu-container'], { 'open': buttonsMenuOpen })}>
-                    <Button className={classnames(styles['control-bar-button'], { 'disabled': statistics === null || statistics.type === 'Err' || stream === null || typeof stream.infoHash !== 'string' || typeof stream.fileIdx !== 'number' })} tabIndex={-1} onMouseDown={onStatisticsButtonMouseDown} onClick={onToggleStatisticsMenu}>
-                        <Icon className={styles['icon']} name={'network'} />
-                    </Button>
+                    {!(
+                        statistics === null ||
+                        statistics.type === 'Err' ||
+                        stream === null ||
+                        typeof stream.infoHash !== 'string' ||
+                        typeof stream.fileIdx !== 'number'
+                    ) && (
+                        <Button
+                            className={styles['control-bar-button']}
+                            tabIndex={-1}
+                            onMouseDown={onStatisticsButtonMouseDown}
+                            onClick={onToggleStatisticsMenu}
+                        >
+                            <Icon className={styles['icon']} name="network" />
+                        </Button>
+                    )}
                     <Button className={classnames(styles['control-bar-button'], { 'disabled': playbackSpeed === null })} tabIndex={-1} onMouseDown={onSpeedButtonMouseDown} onClick={onToggleSpeedMenu}>
                         <Icon className={styles['icon']} name={'speed'} />
                     </Button>
